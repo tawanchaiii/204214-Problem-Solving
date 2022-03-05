b8ba1080a440b6af4e54671e936bdac9e5808017
tawanchaiii <tawanchai_champ@hotmail.com>
2022-03-05 23:15:31 +0700

    wavio

--- week10/cutting_stick.cpp
+++ week10/cutting_stick.cpp
@@ -6,7 +6,7 @@ int main(){
         cin >> l;
         if(!l) return 0;
         cin >> n;
-        int stick[55],a,st,ed,mid;
+        int stick[55]={0},a,st,ed,mid;
         int dp[55][55] = {};
         for(int i=1;i<=n;i++){
             cin >> stick[i];
@@ -14,7 +14,7 @@ int main(){
         stick[++n] = l;
         for(a=2;a<=n;a++){
             for(st=0,ed=a+st;ed<=n;st++,ed++){
-                    int minn = 1e9,t;
+                    int minn = INT_MAX,t;
                     for(mid = st+1;mid<ed;mid++){
                         t = dp[st][mid] + dp[mid][ed] + (stick[ed] - stick[st]);
                         if(t<minn) minn = t;
@@ -23,6 +23,6 @@ int main(){
             }
            
         }
-        printf("The minimum cutting is %mid.\n",dp[0][n]);
+        printf("The minimum cutting is %d.\n",dp[0][n]);
     }
 }
--- week10/wavio.cpp
+++ week10/wavio.cpp
@@ -1,11 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+int A[20010],st[20010],ed[20010],n;
 int main(){
     while(cin >> n){
-        int A[n+1];
-        for(int i=0;i<n;i++){
+        vector<int>vecst,veced;
+        for(int i=1;i<=n;i++){
             cin >> A[i];
+            auto pos = lower_bound(vecst.begin(),vecst.end(),A[i])-vecst.begin();
+            if(pos == vecst.size())
+                vecst.push_back(A[i]);
+            else
+                vecst[pos] = A[i];
+            st[i] = vecst.size();
         }
-        
+        for(int i=n;i>=1;i--){
+            auto pos = lower_bound(veced.begin(),veced.end(),A[i])-veced.begin();
+            if(pos == veced.size())
+                veced.push_back(A[i]);
+            else
+                veced[pos] = A[i];
+            ed[i] = veced.size();
+        }
+        int ans = -1;
+        for(int i=1;i<=n;i++){
+            if(st[i] == ed[i])
+                ans = max(ans,st[i]*2-1);
+        }
+        cout << ans << "\n";
     }
 }
